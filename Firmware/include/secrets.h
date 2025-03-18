9f4cea809367ed8eb694f2a3ed32dcf36cc7f79a
atomoinnovations <atomoinnovations@gmail.com>
2025-03-19 03:24:24 +0530

    Fix bug

--- Firmware/include/secrets.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-constexpr auto WIFI_SSID = "Upeya-2-4";
-constexpr auto WIFI_PASSWORD = "asdfghjkl";
--- Firmware/src/ui/matter_wrapper.cpp
+++
@@ -1,7 +0,0 @@
-#include <Matter.h>
-
-extern "C" {
-    bool is_matter_commissioned() {
-        return ArduinoMatter::isDeviceCommissioned();
-    }
-}
